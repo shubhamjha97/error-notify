584a81e48ec97cf51e6ddecc2abcb446e92cafe0
Shubham Jha <jha1shubham@gmail.com>
2018-11-14 02:15:34 +0530

    change name of custom excepthook

--- error_notify/total_override.py
+++ error_notify/total_override.py
@@ -3,9 +3,9 @@ import os, sys
 def placeholder_notific_fn():
 	print("PLACEHOLDER CALLED")
 
-def Notify(type_, value, traceback):
+def CustomExceptHook(type_, value, traceback):
 	placeholder_notific_fn() # call notification function
 	tempExceptHook(type_, value, traceback)
 
 tempExceptHook = sys.excepthook
-sys.excepthook = Notify
+sys.excepthook = CustomExceptHook
